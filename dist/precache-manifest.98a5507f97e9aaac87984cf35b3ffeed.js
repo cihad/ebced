87f09e680c5e5147e42fe348028b25b58d9e3c14
Cihad Paksoy <cihadp@gmail.com>
2020-05-21 20:08:49 +0300

    build for site

---
+++ dist/precache-manifest.98a5507f97e9aaac87984cf35b3ffeed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97f4a4062a746ba60a8e",
+    "url": "/css/app.0180041d.css"
+  },
+  {
+    "revision": "eb6b6ef02c443c39f126dc1203ba1bca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97f4a4062a746ba60a8e",
+    "url": "/js/app.fe4b310c.js"
+  },
+  {
+    "revision": "41528daceafc61d99fcb",
+    "url": "/js/chunk-vendors.7789fe90.js"
+  },
+  {
+    "revision": "0c463d690fd23082ff55d8b58e710e77",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
